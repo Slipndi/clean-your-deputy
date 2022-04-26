7dfc4cd62b8f2da83e2ef259f36c8edcdea23f71
Ludovic JORGE DO MARCO <ludovic.jorgedomarco@gmail.com>
2022-04-26 06:55:54 +0200

    Création de la logique de route

--- clean-your-deputy/__init__.py
+++ clean-your-deputy/__init__.py
@@ -6,21 +6,25 @@ from flask import Flask, render_template
 def create_app(test_config=None) -> Flask:
     # Création et configuration de l'application
     app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-    )
-    
-    if test_config is None:
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        app.config.from_mapping(test_config)
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
+    app.config.from_pyfile('config.py', silent=True)
 
-    @app.route('/')
+    @app.route('/', methods=['GET'])
     def index():
         return render_template('index.html')
 
+    @app.route('/deputies', methods=['GET'])
+    def get_al_deputies():
+        return True
+    
+    @app.route('/deputy/<deputy_id>')
+    def get_deputy(deputy_id):
+        return deputy_id
+    
+    @app.route('/political-parties', methods=['GET'])
+    def get_all_political_parties():
+        return True
+    
+    @app.route('/political-party/<political_party_id>')
+    def get_political_party(political_party_id):
+        return True    
     return app
